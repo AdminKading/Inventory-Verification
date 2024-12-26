8d806da2d35d667ef5d1032589c4292bc66fbf9a
Matthew Hill <matthill.go@gmail.com>
2024-12-26 06:46:47 -0600

    Added most stuff

--- home.js
+++ home.js
@@ -1,20 +1,4 @@
 window.onload = () => {
-
-    // Add event listener to Current Inventory button
-    const systemInventoryBtn = document.getElementById('system-inventory');
-    if (systemInventoryBtn) {
-        systemInventoryBtn.addEventListener('click', () => {
-            window.location.href = 'currentinventory.html';
-        });
-    }
-    const InventoryCountBtn = document.getElementById('inventory-count');
-    if (InventoryCountBtn) {
-        InventoryCountBtn.addEventListener('click', () => {
-            window.location.href = 'inventorycount.html';
-        });
-    }
-
-
     // Add event listener to Exit button
     const exitButton = document.getElementById('exit');
     if (exitButton) {
