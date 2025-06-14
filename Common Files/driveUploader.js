beb27fda01515ce654f79adcdf34e4193f60f122
Matthew Hill <162880062+mattnyte@users.noreply.github.com>
2025-06-13 21:02:58 -0500

    Fixed Google Scripts

--- Common Files/driveUploader.js
+++ Common Files/driveUploader.js
@@ -4,7 +4,7 @@ export function sendToGoogleDrive(blob, fileName) {
     reader.onload = () => {
         const base64Data = reader.result.split(',')[1];
 
-        fetch('https://script.google.com/macros/s/AKfycbzPEB_YBtEeO7GtroEdP4k_iTzzrxl3infeeif7aMakLQbAsVFg_-MTQd-WWw8UH_xAvw/exec', { // ← Replace with your script URL
+        fetch('https://script.google.com/macros/s/AKfycbybB2ERdFs4UMg1wQCYYoq_CPo35gbWqdZ3B8HMi_QurN09R8GJ3c1QfWQdw0-9AUk8-g/exec', { // ← Replace with your script URL
             method: 'POST',
             headers: {
                 'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
