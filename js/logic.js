e893aecf0abe958f0263b5dbdf564a40e2c55e39
Matthew Hill <162880062+mattnyte@users.noreply.github.com>
2025-06-22 10:16:20 -0500

    Added invalid names

--- js/logic.js
+++ js/logic.js
@@ -2,7 +2,11 @@ const INVALID_NAMES = new Set([
     "chainsaws", "dewalt light", "drill (impact)", "drill (regular)", "multi-tool",
     "post hole diggers", "push lawn mower", "sawzall", "skillsaw", "snow blower",
     "snow shovels", "speed bumps", "sprayer backpacks", "stihl bf-km",
-    "stihl blowers", "weed whips"
+    "stihl blowers", "weed whips", "Hedge Trimmper", "Kubota Lawnmower", "Mailbox Lock C9100", "Pole Sow Urbandale",
+    "Z-Spray Max", "10 Gallon Dogipot Trashbags (1unit=1box)", "42 Gallon Wing-Tie Contractor Trash Bags", 
+    "Master key locking mechanisms (deadbolts)", "Trim Nailer", "Jelly Jar replacement", "6 ft orange ladder", 
+    "6ft tall yellow scaffolding", "Post pounder", "Push broom", "Shop Vac small", "Shop-Vac large", 
+    "Sledgehammer", "Small Pick-up Dogipot Bags (1 unit = 1 roll)", "Snow plow", "Timmer-235E", "Snow Plow #47E & 105E"
 ]);
 
 export const filterValidRows = (rows, mode = 'Inventory') => {
